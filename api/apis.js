d2ae47966d858e571fc06c38da8fbf88585bf026
kangcw <kangcw0107@gmail.com>
2024-06-30 06:14:30 +0900

    Feat : 카테고리별 메뉴 리스트 호출 API

--- api/apis.js
+++ api/apis.js
@@ -1,5 +1,23 @@
-baseURL = "";
+const baseURL = process.env.NEXT_PUBLIC_NEXTAPI_URL;
 
 async function getAllMenu() {
-    const res = await fetch()
+  try {
+    const res = await fetch(baseURL + "/menu");
+    const result = await res.json();
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
 }
+
+async function getMenuByCategory(name) {
+    try {
+      const res = await fetch(baseURL + `/menu/${name}`);
+      const result = await res.json();
+      return result;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+export { getAllMenu,getMenuByCategory };
