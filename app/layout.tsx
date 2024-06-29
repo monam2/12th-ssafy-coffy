7b9620226a88e96643fea2d302990b4fde2bcc28
kangcw <kangcw0107@gmail.com>
2024-06-30 08:31:22 +0900

    Style : 불필요 코드 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
