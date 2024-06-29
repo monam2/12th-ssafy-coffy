de295cab0805cea57c772bd55cdf7d8516c447ab
kangcw <kangcw0107@gmail.com>
2024-06-30 06:12:27 +0900

    Feat : API - 카테고리 선택에 따른 메뉴 리스트 API

---
+++ app/api/menu/[category]/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import menuData from "../../../../data/menu.json";
+import categoryData from "../../../../data/category.json";
+
+interface MenuObject {
+  id: number;
+  category: string;
+  menu: string;
+  onlyIce: boolean;
+  price: number;
+  img: string;
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { category: string } }
+) {
+  const { category } = params;
+  console.log(category);
+  const menuList: MenuObject[] = menuData.data as MenuObject[];
+
+  const menus = menuList.filter(item => item.category === category);
+
+  const response = NextResponse.json(menus);
+  response.headers.set("Content-Type", "application/json; charset=utf-8");
+
+  return response;
+}
