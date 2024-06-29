f87b1d32f9cff14157e3c6eba29443b540b735c2
kangcw <kangcw0107@gmail.com>
2024-06-30 06:16:41 +0900

    Style : 메뉴 스크롤바 추가

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -2,7 +2,7 @@ import Menu from "@/components/page/menu/Menu";
 import React from "react";
 
 const page = () => {
-  return <div className="w-full h-full">
+  return <div className="w-full h-full overflow-y-auto" style={{scrollbarWidth : "thin"}}>
     <Menu />
   </div>;
 };
