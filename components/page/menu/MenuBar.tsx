7be6b133859aa6cca86a37cb71ede31b224f1e63
kangcw <kangcw0107@gmail.com>
2024-06-30 06:13:49 +0900

    Feat : 메뉴바 가로 스크롤 추가 및 카테고리 목록에 따라 렌더링

---
+++ components/page/menu/MenuBar.tsx
@@ -0,0 +1,43 @@
+import React, { forwardRef } from "react";
+
+interface categoryObject {
+  id: number;
+  value: string;
+  name:string
+}
+
+interface MenuBarProps {
+  selectedCategory: string;
+  category: categoryObject[];
+  setSelectedCategory: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const MenuBar = forwardRef<HTMLDivElement, MenuBarProps>(
+  ({ selectedCategory, category, setSelectedCategory }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="w-full px-3 md:justify-center justify-start flex gap-5 overflow-x-scroll whitespace-nowrap"
+        style={{ scrollbarWidth: "none" }}
+      >
+        {category.map((item) => (
+          <div
+            onClick={() => {
+              setSelectedCategory(item.value);
+            }}
+            className={`p-2 cursor-pointer text-center font-semibold text-lg font-[Pretendard] ${
+              selectedCategory === item.value
+                ? "bg-sky-300 rounded-2xl dark:bg-sky-500"
+                : "bg-none"
+            }`}
+            key={item.id}
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
+    );
+  }
+);
+
+export default MenuBar;
