d992ad943f2a86920e684ecf91c9c0a256920fb0
kangcw <kangcw0107@gmail.com>
2024-06-30 01:19:37 +0900

    Feat : 사용자 입력 검증 추가

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -13,40 +13,18 @@ export default function Home() {
   const [mmId, setMmId] = React.useState("");
   const router = useRouter();
 
-  const isValidKoreanName = () => {
-    const nameLengthRegex = /^[가-힣]{2,5}$/;
-    const validKoreanNameRegex = /^[가-힣]+$/;
-
-    if (!name || name === "" || name === null) {
-      return false;
-    }
-
-    if (!nameLengthRegex.test(name)) {
-      return false;
-    }
-
-    if (!validKoreanNameRegex.test(name)) {
-      return false;
-    }
-
-    return true;
-  };
-
-  const isValidMmId = () => {
-    // '@'가 포함되어 있는지 검사
-    if (mmId.includes("@")) {
-      return false;
-    }
-    return true;
-  };
-
   const onClickHandler = () => {
     router.push("/menu");
   };
 
   return (
     <div className="flex flex-col justify-center gap-10 items-center h-full">
-      <Image src="/img/logo/logo.png" alt="logo" width={220} height={500}></Image>
+      <Image
+        src="/img/logo/logo.png"
+        alt="logo"
+        width={220}
+        height={500}
+      ></Image>
       <div className="flex flex-col gap-4">
         <Input
           placeholder="주문자 명을 입력하세요."
@@ -58,13 +36,16 @@ export default function Home() {
           className="w-[220px] --point-color text-base font-medium text-center"
           onChange={(event) => setMmId(event.target.value)}
         />
-        <span className="text-sm text-center text-gray-500 mb-5">(MM프로필 클릭시 보이는 @ID)</span>
+        <span className="text-sm text-center text-gray-500 mb-5">
+          (MM프로필 클릭시 보이는 @ID)
+        </span>
         <Combobox setClassNumber={setClassNumber} />
         <div className="w-[220px] h-12 --text-color text-lg">
           <CustomAlertDialog
             onClickHandler={onClickHandler}
             classNumber={classNumber}
             name={name}
+            mmId={mmId}
             value={"메뉴 보러가기"}
           />
         </div>
