fe1b0e7c780df46c45fc59f5c47dc0ed69d12fa7
kangcw <kangcw0107@gmail.com>
2024-07-01 02:03:05 +0900

    Fix : react-toastify 관련 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'composecoffee.com',
-            port: '',
-            pathname: '/files/thumbnails/**',
-          },
-        ],
+  assetPrefix: ".",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "composecoffee.com",
+        port: "",
+        pathname: "/files/thumbnails/**",
       },
-      webpack: (config, { dev, isServer }) => {
-        if (dev && !isServer) {
-          config.devtool = 'eval-source-map';
-        }
-        return config;
-      },
+    ],
+  },
 };
 
 export default nextConfig;
