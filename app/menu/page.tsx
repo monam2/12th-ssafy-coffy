5f4ecf08188166129aac3ecec2026fd1e35e3d9c
kangcw <kangcw0107@gmail.com>
2024-06-30 19:42:06 +0900

    Feat : 메뉴 페이지 Top 버튼 추가

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,10 +1,35 @@
+"use client"
+import React, { useRef } from "react";
 import Menu from "@/components/page/menu/Menu";
-import React from "react";
+import { FaArrowUp } from "react-icons/fa";
 
-const page = () => {
-  return <div className="w-full h-full overflow-y-auto" style={{scrollbarWidth : "thin"}}>
-    <Menu />
-  </div>;
+const Page = () => {
+  const menuContainerRef = useRef<HTMLDivElement>(null);
+
+  const scrollToTop = () => {
+    if (menuContainerRef.current) {
+      menuContainerRef.current.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <div
+      className="w-full h-full overflow-y-auto"
+      style={{ scrollbarWidth: "thin" }}
+      ref={menuContainerRef}
+    >
+      <Menu scrollToTop={scrollToTop} />
+      <button
+        onClick={scrollToTop}
+        className="fixed bottom-20 right-6 bg-blue-300 p-3 text-white rounded-full shadow-lg"
+      >
+        <FaArrowUp className="w-6 h-6" />
+      </button>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
