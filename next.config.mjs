ea7a8c1e8582026e781394f3c855a5b8e4b17b6a
kangcw <kangcw0107@gmail.com>
2024-06-30 19:41:44 +0900

    Fix : ReactToastify 소스맵 파일 사용 안하도록 설정

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
           },
         ],
       },
+      webpack: (config, { dev, isServer }) => {
+        if (dev && !isServer) {
+          config.devtool = 'eval-source-map';
+        }
+        return config;
+      },
 };
 
 export default nextConfig;
