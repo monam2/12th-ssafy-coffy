41bb86ac318e83dc5340ddccd08e154f4e14bc51
kangcw <kangcw0107@gmail.com>
2024-07-01 02:20:05 +0900

    Feat : 장바구니 열기 기능 추가 및 Recoil - cart 연동

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,10 +1,15 @@
-"use client"
-import React, { useRef } from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import Menu from "@/components/page/menu/Menu";
 import { FaArrowUp } from "react-icons/fa";
+import CartModal from "@/components/page/cart/CartModal";
+import { IoCartSharp } from "react-icons/io5";
+import { isOpenCartState } from "@/recoil/cart/atoms";
+import { useSetRecoilState } from "recoil";
 
 const Page = () => {
   const menuContainerRef = useRef<HTMLDivElement>(null);
+  const setIsOpenCart = useSetRecoilState(isOpenCartState);
 
   const scrollToTop = () => {
     if (menuContainerRef.current) {
@@ -24,10 +29,17 @@ const Page = () => {
       <Menu scrollToTop={scrollToTop} />
       <button
         onClick={scrollToTop}
-        className="fixed bottom-20 right-6 bg-blue-300 p-3 text-white rounded-full shadow-lg"
+        className="fixed bottom-20 right-6 bg-blue-300 hover:bg-blue-500 hover:scale-110 p-3 text-white rounded-full shadow-lg"
       >
         <FaArrowUp className="w-6 h-6" />
       </button>
+      <button
+      onClick={()=>setIsOpenCart(true)}
+        className="fixed bottom-20 right-20 bg-blue-300 hover:bg-blue-500 hover:scale-110 p-3 text-white rounded-full shadow-lg"
+      >
+        <IoCartSharp className="w-6 h-6"/>
+      </button>
+      <CartModal />
     </div>
   );
 };
