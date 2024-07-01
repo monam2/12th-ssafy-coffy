118645d1007ca9de9a3c9ae64d6077ddeffc388f
kangcw <kangcw0107@gmail.com>
2024-07-02 03:03:48 +0900

    Style : 로딩 컴포넌트 수정

--- components/elements/Loading.tsx
+++ components/elements/Loading.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import {BarLoader} from "react-spinners"
-
+"use client";
+import React from "react";
+import { ClipLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <BarLoader/>
-  )
-}
+    <div className="w-full h-full flex flex-col justify-center items-center gap-8">
+      <ClipLoader size={120} color="#8a8a8a" />
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
