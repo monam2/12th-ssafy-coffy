033ee0e033def38c5513db6bd7a1c62000e05a71
kangcw <kangcw0107@gmail.com>
2024-07-02 03:06:07 +0900

    Feat : 송금 버튼 및 카카오페이 송금 링크로 이동

--- app/menu/order/page.tsx
+++ app/menu/order/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { v4 as uuidv4 } from "uuid";
 import CartList from "@/components/page/cart/CartList";
 import { totalCartPrice } from "@/recoil/cart/selector";
 import { userState } from "@/recoil/user/atoms";
@@ -7,11 +8,40 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useRecoilValue } from "recoil";
 import { ClipLoader } from "react-spinners";
+import { postOrder } from "@/api/firebase/index";
+import { cartState } from "@/recoil/cart/atoms";
+
+interface cartDto {
+  id: number;
+  category: string;
+  menu: string;
+  onlyIce: boolean;
+  price: number;
+  img: string;
+  isShot: boolean;
+  isWhip: boolean;
+  isSyrup: boolean;
+  isMilk: boolean;
+  isPeorl: boolean;
+  isHot: boolean;
+  cartId: number;
+}
+
+interface orderDto {
+  orderId: string;
+  classNum: number;
+  mmId: string;
+  user: string;
+  createdAt?: Date;
+  menus: cartDto[];
+  totalPrice : number;
+}
 
 const Page = () => {
   const router = useRouter();
   const totalPrice = useRecoilValue(totalCartPrice);
-  const user = useRecoilValue(userState);
+  const { name, mmId, classNum } = useRecoilValue(userState);
+  const cartItems = useRecoilValue(cartState);
   const [isLoading, setIsLoading] = useState(false);
   const [isOrdered, setIsOrdered] = useState(false);
 
@@ -24,8 +54,17 @@ const Page = () => {
     }, 5000);
   };
 
-  const moveTo = () => {
-    router.push("check");
+  const successPayment = () => {
+    const order: orderDto = {
+      orderId: uuidv4(),
+      totalPrice,
+      user: name,
+      mmId,
+      classNum,
+      menus: cartItems,
+    };
+    postOrder(order);
+    router.push("/list");
   };
 
   if (isLoading) {
@@ -47,7 +86,7 @@ const Page = () => {
       </div>
       <div className="flex flex-col gap-6 items-center justify-center">
         <span className="font-[Pretendard] text-2xl font-semibold text-center">
-          {user.classNum}반 {user.name}님의 주문내역
+          {classNum}반 {name}님의 주문내역
         </span>
         <span className="font-[Pretendard] text-2xl font-semibold text-center">
           총 {totalPrice.toLocaleString()}원
@@ -65,7 +104,7 @@ const Page = () => {
           className={`${
             !isOrdered ? "hidden" : ""
           } w-[200px] font-[Pretendard] h-[50px] text-lg rounded-3xl bg-blue-400 text-white`}
-          onClick={moveTo}
+          onClick={successPayment}
         >
           송금 완료!
         </button>
