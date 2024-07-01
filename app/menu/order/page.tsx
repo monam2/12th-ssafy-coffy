cac8c22fe5d90ee8bdb9f515d7e64a7b6d63db44
kangcw <kangcw0107@gmail.com>
2024-07-01 11:37:32 +0900

    Feat : 카카오페이 코드송금 링크 추가

--- app/menu/order/page.tsx
+++ app/menu/order/page.tsx
@@ -9,6 +9,10 @@ import { useRecoilValue } from "recoil";
 const Page = () => {
   const totalPrice = useRecoilValue(totalCartPrice);
   const user = useRecoilValue(userState);
+
+  const onClickHandler = ()=> {
+    window.open(`${process.env.NEXT_PUBLIC_KAKAOPAY_LINK}`)
+  }
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-8">
       <div className="bg-slate-100 dark:bg-slate-500 rounded-lg px-3 py-5 shadow-lg">
@@ -17,7 +21,7 @@ const Page = () => {
       <div className="flex flex-col gap-6 items-center justify-center">
         <span className="font-[Pretendard] text-2xl font-semibold text-center">{user.classNum}반 {user.name}님의 주문내역</span>
         <span className="font-[Pretendard] text-2xl font-semibold text-center">총 {totalPrice.toLocaleString()}원</span>
-        <Image src="/img/pay_btn_regular.png" alt="pay" width={200} height={50} className="cursor-pointer"/>
+        <Image src="/img/pay_btn_regular.png" alt="pay" width={200} height={50} className="cursor-pointer" onClick={onClickHandler}/>
       </div>
     </div>
   );
