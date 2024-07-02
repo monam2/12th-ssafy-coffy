4d3eec4b5f0bfaff8f01828f825d85083048eb81
monam2 <kangcw0107@gmail.com>
2024-07-02 17:45:54 +0900

    Style : 최종 주문 페이지 레이아웃 수정

--- app/menu/order/page.tsx
+++ app/menu/order/page.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { v4 as uuidv4 } from "uuid";
 import CartList from "@/components/page/cart/CartList";
 import { totalCartPrice } from "@/recoil/cart/selector";
 import { userState } from "@/recoil/user/atoms";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useRecoilValue } from "recoil";
 import { ClipLoader } from "react-spinners";
 import { postOrder } from "@/api/firebase/index";
 import { cartState } from "@/recoil/cart/atoms";
+import OrderModal from "@/components/page/order/OrderModal";
 
 interface cartDto {
   id: number;
@@ -44,6 +43,7 @@ const Page = () => {
   const cartItems = useRecoilValue(cartState);
   const [isLoading, setIsLoading] = useState(false);
   const [isOrdered, setIsOrdered] = useState(false);
+  const [isOpenPayModal, setIsOpenPayModal] = useState(false);
 
   const onClickHandler = () => {
     window.open(`${process.env.NEXT_PUBLIC_KAKAOPAY_LINK}`);
@@ -55,16 +55,17 @@ const Page = () => {
   };
 
   const successPayment = () => {
-    const order: orderDto = {
-      orderId: uuidv4(),
-      totalPrice,
-      user: name,
-      mmId,
-      classNum,
-      menus: cartItems,
-    };
-    postOrder(order);
-    router.push("/list");
+    setIsOpenPayModal(true);
+    // const order: orderDto = {
+    //   orderId: uuidv4(),
+    //   totalPrice,
+    //   user: name,
+    //   mmId,
+    //   classNum,
+    //   menus: cartItems,
+    // };
+    // postOrder(order);
+    // router.push("/list");
   };
 
   if (isLoading) {
@@ -78,9 +79,7 @@ const Page = () => {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-8">
       <div
-        className={`${
-          !isOrdered ? "" : "hidden"
-        } bg-slate-100 dark:bg-slate-500 rounded-lg px-3 py-5 shadow-lg`}
+        className='bg-slate-100 dark:bg-slate-500 rounded-lg px-3 py-5 shadow-lg'
       >
         <CartList />
       </div>
@@ -91,42 +90,20 @@ const Page = () => {
         <span className="font-[Pretendard] text-2xl font-semibold text-center">
           총 {totalPrice.toLocaleString()}원
         </span>
-        <Image
-          src="/img/pay_btn_regular.png"
-          alt="pay"
-          width={200}
-          height={50}
-          className={`${!isOrdered ? "" : "hidden"} cursor-pointer`}
-          onClick={onClickHandler}
-        />
-
         <button
-          className={`${
-            !isOrdered ? "hidden" : ""
-          } w-[200px] font-[Pretendard] h-[50px] text-lg rounded-3xl bg-blue-400 text-white`}
+          className='w-[200px] font-[Pretendard] h-[50px] text-lg rounded-3xl bg-blue-400 text-white'
           onClick={successPayment}
         >
-          송금 완료!
+          송금하기
         </button>
-        <button
-          className={`${
-            !isOrdered ? "hidden" : ""
-          } w-[200px] font-[Pretendard] h-[50px] text-lg rounded-3xl bg-red-300 text-white`}
-          onClick={() => setIsOrdered(false)}
-        >
-          송금에 실패했어요
-        </button>
-        <span className={`font-[Pretendard] mt-5 text-md text-gray-400`}>
-          송금 후 반드시 [송금완료] 버튼을 눌러야 주문 목록에 반영됩니다.
-        </span>
         <span
-          className={`${
-            !isOrdered ? "hidden" : ""
-          } font-[Pretendard] text-sm text-gray-400`}
+          className='font-[Pretendard] text-sm text-gray-400'
         >
-          송금이 계속 실패할 경우 하단 버튼을 클릭해 MM 메시지 보내주세요.
+          송금에 실패할 경우 하단 버튼을 클릭해 MM 메시지 보내주세요.
         </span>
       </div>
+      <OrderModal isOpenPayModal={isOpenPayModal} setIsOpenPayModal={setIsOpenPayModal}/>
+      
     </div>
   );
 };
