4b943289bb5b35cac2240dbd3fa5d8f3030cf0a6
monam2 <kangcw0107@gmail.com>
2024-07-03 23:41:11 +0900

    Feat : Session Storage의 장바구니 목록에 상품이 없으면 결제(송금) 불가하도록 설정

--- app/menu/order/page.tsx
+++ app/menu/order/page.tsx
@@ -1,35 +1,46 @@
 "use client";
 import CartList from "@/components/page/cart/CartList";
 import { totalCartPrice } from "@/recoil/cart/selector";
-import { userState } from "@/recoil/user/atoms";
 import React, { useEffect, useState } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import OrderModal from "@/components/page/order/OrderModal";
 import { checkUser } from "@/lib/checkUser";
-import { cartState } from "@/recoil/cart/atoms";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
   const totalPrice = useRecoilValue(totalCartPrice);
   const [name, setName] = useState("");
   const [mmId, setMmId] = useState("");
   const [classNum, setClassNum] = useState(1);
-  const [cartData, setCartData] = useRecoilState(cartState);
-  const [isOpenPayModal, setIsOpenPayModal] = useState(false);  
+  const [isOpenPayModal, setIsOpenPayModal] = useState(false);
 
-  const decryptUserData = ()=> {
-    const decryptedUser:{decryptedName:string, decryptedMmid:string, decryptedClassNum:string} | null = checkUser();
+  const decryptUserData = () => {
+    const decryptedUser: { decryptedName: string, decryptedMmid: string, decryptedClassNum: string } | null = checkUser();
 
-    if (decryptedUser){
-      const {decryptedName,decryptedMmid,decryptedClassNum} = decryptedUser;
+    if (decryptedUser) {
+      const { decryptedName, decryptedMmid, decryptedClassNum } = decryptedUser;
       setName(decryptedName);
       setMmId(decryptedMmid);
       setClassNum(+decryptedClassNum);
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     decryptUserData();
-  },[])
+  }, [])
+
+  const handlePayButtonClick = () => {
+    const cartData = window.sessionStorage.getItem('cart');
+    if (!cartData) {
+      alert(
+        `장바구니에 담긴 상품이 없습니다.
+메뉴 페이지로 이동합니다.`);
+      router.push('/menu')
+      return;
+    }
+    setIsOpenPayModal(true);
+  };
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-8">
@@ -47,18 +58,18 @@ const Page = () => {
         </span>
         <button
           className='w-[200px] font-[Pretendard] h-[50px] text-lg rounded-3xl bg-blue-400 text-white'
-          onClick={()=>setIsOpenPayModal(true)}
+          onClick={handlePayButtonClick}
         >
           송금하기
         </button>
         <span
           className='mt-5 font-[Pretendard] text-base text-center text-gray-400'
         >
-          잔액 부족 등으로 송금에 실패했다면<br/> 하단 버튼으로 MM 메세지 보내주세요.
+          잔액 부족 등으로 송금에 실패했다면<br /> 하단 버튼으로 MM 메세지 보내주세요.
         </span>
       </div>
-      <OrderModal isOpenPayModal={isOpenPayModal} setIsOpenPayModal={setIsOpenPayModal}/>
-      
+      <OrderModal isOpenPayModal={isOpenPayModal} setIsOpenPayModal={setIsOpenPayModal} />
+
     </div>
   );
 };
